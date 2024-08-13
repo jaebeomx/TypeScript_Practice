d59a26b21e6f980ba5335a9d76f1d8f09bbcc28a
jaebeom <sukjb80@gmail.com>
2024-08-13 15:41:55 +0900

    enum 타입까지 학습완료

---
+++ onebite-typescript/section2/src/chapter3.ts
@@ -0,0 +1,15 @@
+// object
+let user: { id?: number; name: string } = {
+  id: 1,
+  name: "석재범",
+};
+
+user = {
+  name: "홍길동",
+};
+
+let config: { readonly apiKey: string } = {
+  apiKey: "MY API KEY",
+};
+
+// config.apiKey = "new API";
